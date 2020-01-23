553c14e988735c857c22716efc8fe1b659f2939f
nikolavojicic <nikolavojicic@outlook.com>
2020-01-23 14:37:30 +0100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject belit/diff-rdb "0.1.0-SNAPSHOT"
+  :description "Tools and APIs for finding, inspecting and fixing differences between data sets in relational databases."
+  :url "https://github.com/belit-rs/diff-rdb"
+  :license {:url "https://github.com/belit-rs/diff-rdb/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
