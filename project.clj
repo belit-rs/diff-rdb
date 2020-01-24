33a21566c493b4b2abd196dbe8ace3a5efea5db8
nikolavojicic <nikolavojicic@outlook.com>
2020-01-24 17:15:23 +0100

    Add copyright notice

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
+;; Copyright (c) Belit d.o.o.
+
+
 (defproject belit/diff-rdb "0.1.0-SNAPSHOT"
   :description "Tools and APIs for finding, inspecting and fixing differences between data sets in relational databases."
   :url "https://github.com/belit-rs/diff-rdb"
