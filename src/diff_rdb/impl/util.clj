b716aea6722aeab5c32bb7900f0bc7a1df42b484
nikolavojicic <nikolavojicic@outlook.com>
2020-04-22 14:42:56 +0200

    Add util

---
+++ src/diff_rdb/impl/util.clj
@@ -0,0 +1,28 @@
+;; Copyright (c) Belit d.o.o.
+
+
+(ns diff-rdb.impl.util
+  (:require
+   [clojure.string :as str]))
+
+
+(defn equalize-ptn
+  "If the number of items in ptn partition is less
+  than size, the first item will be re-added into
+  it n times to make it equal."
+  [size ptn]
+  (let [n (- size (count ptn))]
+    (if (zero? n)
+      ptn
+      (->> (first ptn)
+           (repeat n)
+           (into ptn)))))
+
+
+(defn expand-?s
+  "Replaces '?s' in a query with n times repeated '?',
+  separated by the comma separator."
+  [n query]
+  (->> (repeat n \?)
+       (str/join \,)
+       (str/replace query "?s")))
---
+++ test/diff_rdb/impl/util_test.clj
@@ -0,0 +1,36 @@
+;; Copyright (c) Belit d.o.o.
+
+
+(ns diff-rdb.impl.util-test
+  (:require
+   [clojure.test :refer :all]
+   [clojure.string :as str]
+   [diff-rdb.impl.util :as impl]))
+
+
+(deftest equalize-ptn-test
+  (is (= (impl/equalize-ptn 5 [1])
+         [1 1 1 1 1]))
+  (is (= (impl/equalize-ptn 5 [1 2 3])
+         [1 2 3 1 1]))
+  (testing "Trash cases"
+    (is (= (impl/equalize-ptn -5 [1 2 3])
+           (impl/equalize-ptn  0 [1 2 3])
+           (impl/equalize-ptn  2 [1 2 3])
+           (impl/equalize-ptn  3 [1 2 3])
+           [1 2 3]))
+    (is (= (impl/equalize-ptn 3 [])
+           (impl/equalize-ptn 3 nil)
+           [nil nil nil]))
+    (is (= (impl/equalize-ptn -5 []) []))
+    (is (= (impl/equalize-ptn -5 nil) nil))))
+
+
+(deftest expand-?s-test
+  (is (= (impl/expand-?s 3 "IN(?s)")
+         "IN(?,?,?)"))
+  (testing "Trash cases"
+    (is (= (impl/expand-?s -3 "IN(?s)")
+           (impl/expand-?s  0 "IN(?s)")
+           (impl/expand-?s  3 "IN()")
+           "IN()"))))
