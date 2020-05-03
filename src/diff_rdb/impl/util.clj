08555d5590319c1a966fcb39efa9def4ab2f83df
nikolavojicic <nikolavojicic@outlook.com>
2020-05-03 02:07:57 +0200

    Add ptn-xf

--- src/diff_rdb/impl/util.clj
+++ src/diff_rdb/impl/util.clj
@@ -19,6 +19,16 @@
            (into ptn)))))
 
 
+(defn ptn-xf
+  "Returns a transducer that transforms incoming
+  collections of any size to vectors of size n."
+  [n]
+  (comp
+   (mapcat vec)
+   (partition-all n)
+   (map #(equalize-ptn n %))))
+
+
 (defn expand-?s
   "Replaces '?s' in a query with n times repeated '?',
   separated by the comma separator."
