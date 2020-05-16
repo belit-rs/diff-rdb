e3aac99a64b019b4abeda87779f45c8c234686e7
nikolavojicic <nikolavojicic@outlook.com>
2020-05-17 00:25:20 +0200

    Separate standard and trash cases

--- test/diff_rdb/diff_test.clj
+++ test/diff_rdb/diff_test.clj
@@ -4,6 +4,7 @@
 (ns diff-rdb.diff-test
   (:require
    [clojure.test :refer :all]
+   [clojure.spec.test.alpha :as st]
    [diff-rdb.diff :refer [diff]])
   (:import
    (clojure.lang ArityException)
@@ -19,323 +20,339 @@
    m))
 
 
+(defn test-diff
+  [config src tgt ret]
+  (->> (diff config tgt src)
+       (clojure.walk/postwalk
+        #(if (map? %)
+           (clojure.set/rename-keys
+            % {:ins :del, :del :ins
+               :src :tgt, :tgt :src}) %))
+       (vector ret (diff config src tgt))
+       (map sort-vectors)
+       (apply =)))
+
+
 (deftest diff-test
-  (are [config src tgt _ ret]
-      (->> (diff config tgt src)
-           (clojure.walk/postwalk
-            #(if (map? %)
-               (clojure.set/rename-keys
-                % {:ins :del, :del :ins
-                   :src :tgt, :tgt :src}) %))
-           (vector ret (diff config src tgt))
-           (map sort-vectors)
-           (apply =))
+  (testing "Standard cases"
+    (are [config src tgt _ ret]
+        (test-diff config src tgt ret)
 
-    ;; NONE TO NONE
+      ;; NONE TO NONE
 
-    {:match-by [:foo]} nil nil :=> {}
-    {:match-by [:foo]} []  nil :=> {}
-    {:match-by [:foo]} []  []  :=> {}
+      {:match-by [:foo]}                   [] [] :=> {}
+      {:match-by [:foo] :ponders {:bar 1}} [] [] :=> {}
 
-    {:match-by [:foo] :ponders {:bar 1}} nil nil :=> {}
-    {:match-by [:foo] :ponders {:bar 1}} []  nil :=> {}
-    {:match-by [:foo] :ponders {:bar 1}} []  []  :=> {}
+      ;; SEQ TO NONE
 
-    ;; SEQ TO NONE
+      {:match-by [:foo]}
+      [{:foo 1 :bar 1}]
+      []
+      :=> {:ins [{:foo 1 :bar 1}]}
 
-    {:match-by [:foo]}
-    [{:foo 1 :bar 1}]
-    nil
-    :=> {:ins [{:foo 1 :bar 1}]}
+      {:match-by [:foo]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}]
+      []
+      :=> {:ins [{:foo 1 :bar 1}]}
 
-    {:match-by [:foo]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}]
-    nil
-    :=> {:ins [{:foo 1 :bar 1}]}
+      {:match-by [:foo]}
+      [{:foo 1 :bar 1}
+       {:foo 2 :bar 2}]
+      []
+      :=> {:ins [{:foo 1 :bar 1}
+                 {:foo 2 :bar 2}]}
 
-    {:match-by [:foo]}
-    [{:foo 1 :bar 1}]
-    []
-    :=> {:ins [{:foo 1 :bar 1}]}
+      {:match-by [:foo]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}
+       {:foo 2 :bar 2}]
+      []
+      :=> {:ins [{:foo 1 :bar 1}
+                 {:foo 2 :bar 2}]}
 
-    {:match-by [:foo]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}]
-    []
-    :=> {:ins [{:foo 1 :bar 1}]}
+      ;; ONE TO ONE
 
-    {:match-by [:bar]}
-    [{:foo 1 :bar 1}
-     {:foo 2 :bar 2}]
-    nil
-    :=> {:ins [{:foo 1 :bar 1}
-               {:foo 2 :bar 2}]}
+      {:match-by [:foo]}
+      [{:foo 1 :bar 1}]
+      [{:foo 1 :bar 1}]
+      :=> {}
 
-    {:match-by [:bar]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}
-     {:foo 2 :bar 2}]
-    nil
-    :=> {:ins [{:foo 1 :bar 1}
-               {:foo 2 :bar 2}]}
+      {:match-by [:foo]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}]
+      [{:foo 1 :bar 1}]
+      :=> {}
 
-    {:match-by [:foo]}
-    [{:foo 1 :bar 1}
-     {:foo 2 :bar 2}]
-    []
-    :=> {:ins [{:foo 1 :bar 1}
-               {:foo 2 :bar 2}]}
+      {:match-by [:foo]}
+      [{:foo 1 :bar 1}]
+      [{:foo 1 :bar 2}]
+      :=> {}
 
-    {:match-by [:foo]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}
-     {:foo 2 :bar 2}]
-    []
-    :=> {:ins [{:foo 1 :bar 1}
-               {:foo 2 :bar 2}]}
+      {:match-by [:foo]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}]
+      [{:foo 1 :bar 2}]
+      :=> {:upd [{:src  {:foo 1 :bar 1}
+                  :tgt  {:foo 1 :bar 2}
+                  :cols [:bar]}]}
 
-    ;; ONE TO ONE
+      {:match-by [:foo :bar]}
+      [{:foo 1 :bar 1}]
+      [{:foo 1 :bar 2}]
+      :=> {:ins [{:foo 1 :bar 1}]
+           :del [{:foo 1 :bar 2}]}
 
-    {:match-by [:foo]}
-    [{:foo 1 :bar 1}]
-    [{:foo 1 :bar 1}]
-    :=> {}
+      ;; ONE TO MANY
 
-    {:match-by [:foo]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}]
-    [{:foo 1 :bar 1}]
-    :=> {}
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 1 :bar 1 :baz 2}]
+      :=> {:del [{:id 11 :foo 1 :bar 3 :baz 3}]
+           :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 22 :foo 1 :bar 1 :baz 2}
+                  :cols [:baz]}]}
 
-    {:match-by [:foo]}
-    [{:foo 1 :bar 1}]
-    [{:foo 1 :bar 2}]
-    :=> {}
+      ;; Same ponder sum
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 1 :bar 4 :baz 2}]
+      :=> {:del [{:id 22 :foo 1 :bar 4 :baz 2}]
+           :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
+                  :cols [:bar :baz]}]}
 
-    {:match-by [:foo]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}]
-    [{:foo 1 :bar 2}]
-    :=> {:upd [{:src  {:foo 1 :bar 1}
-                :tgt  {:foo 1 :bar 2}
-                :cols [:bar]}]}
+      {:match-by [:foo]}
+      [{:id  1 :foo 1 :bar 1 :baz 1}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 1 :bar 1 :baz 2}]
+      :=> {:del [{:id 22 :foo 1 :bar 1 :baz 2}]}
 
-    {:match-by [:foo :bar]}
-    [{:foo 1 :bar 1}]
-    [{:foo 1 :bar 2}]
-    :=> {:ins [{:foo 1 :bar 1}]
-         :del [{:foo 1 :bar 2}]}
+      {:match-by [:foo :bar]}
+      [{:id  1 :foo 1 :bar 1 :baz 1}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 1 :bar 1 :baz 2}]
+      :=> {:del [{:id 11 :foo 1 :bar 3 :baz 3}]}
 
-    ;; ONE TO MANY
+      ;; MANY TO MANY
 
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 1 :bar 1 :baz 2}]
-    :=> {:del [{:id 11 :foo 1 :bar 3 :baz 3}]
-         :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 22 :foo 1 :bar 1 :baz 2}
-                :cols [:baz]}]}
+      ;; All eq rows
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
+       {:id  2 :foo 2 :bar 2 :baz 2}]
+      [{:id 11 :foo 1 :bar 1 :baz 1}
+       {:id 22 :foo 2 :bar 2 :baz 2}]
+      :=> {}
 
-    ;; Same ponder sum
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 1 :bar 4 :baz 2}]
-    :=> {:del [{:id 22 :foo 1 :bar 4 :baz 2}]
-         :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
-                :cols [:bar :baz]}]}
-
-    {:match-by [:foo]}
-    [{:id  1 :foo 1 :bar 1 :baz 1}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 1 :bar 1 :baz 2}]
-    :=> {:del [{:id 22 :foo 1 :bar 1 :baz 2}]}
-
-    {:match-by [:foo :bar]}
-    [{:id  1 :foo 1 :bar 1 :baz 1}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 1 :bar 1 :baz 2}]
-    :=> {:del [{:id 11 :foo 1 :bar 3 :baz 3}]}
-
-    ;; MANY TO MANY
-
-    ;; All eq rows
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 2 :baz 2}]
-    [{:id 11 :foo 1 :bar 1 :baz 1}
-     {:id 22 :foo 2 :bar 2 :baz 2}]
-    :=> {}
-
-    ;; Some eq rows
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 2 :baz 2}]
-    [{:id 11 :foo 1 :bar 1 :baz 3}
-     {:id 22 :foo 2 :bar 2 :baz 2}]
-    :=> {:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
-                :cols [:baz]}]}
-
-    ;; No eq rows
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 2 :baz 2}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 2 :bar 4 :baz 2}]
-    :=> {:upd [{:src  {:id  2 :foo 2 :bar 2 :baz 2}
-                :tgt  {:id 22 :foo 2 :bar 4 :baz 2}
-                :cols [:bar]}
-               {:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
-                :cols [:bar :baz]}]}
-
-    ;; Different count
-    {:match-by [:foo]
-     :ponders  {:bar 1 :baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 2 :baz 2}
-     {:id  3 :foo 2 :bar 4 :baz 2}]
-    [{:id 11 :foo 1 :bar 3 :baz 3}
-     {:id 22 :foo 2 :bar 4 :baz 2}]
-    :=> {:ins [{:id 2 :foo 2 :bar 2 :baz 2}]
-         :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
-                :cols [:bar :baz]}]}
-
-    ;; Multiple match-by cols
-    {:match-by [:foo :bar]
-     :ponders  {:baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 1 :baz 2}
-     {:id  3 :foo 2 :bar 4 :baz 2}]
-    [{:id 11 :foo 1 :bar 1 :baz 3}
-     {:id 22 :foo 2 :bar 4 :baz 2}]
-    :=> {:ins [{:id 2 :foo 2 :bar 1 :baz 2}]
-         :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
-                :cols [:baz]}]}
-
-    ;; Duplicate rows
-    {:match-by [:foo :bar]
-     :ponders  {:baz 2}}
-    [{:id  1 :foo 1 :bar 1 :baz 1}
-     {:id  2 :foo 2 :bar 1 :baz 2}
-     {:id  2 :foo 2 :bar 1 :baz 2}
-     {:id  2 :foo 2 :bar 1 :baz 2}]
-    [{:id 11 :foo 1 :bar 1 :baz 3}
-     {:id 22 :foo 2 :bar 4 :baz 2}
-     {:id 33 :foo 2 :bar 1 :baz 2}]
-    :=> {:ins [{:id  2 :foo 2 :bar 1 :baz 2}
-               {:id  2 :foo 2 :bar 1 :baz 2}]
-         :del [{:id 22 :foo 2 :bar 4 :baz 2}]
-         :upd [{:src {:id  1 :foo 1 :bar 1 :baz 1}
-                :tgt {:id 11 :foo 1 :bar 1 :baz 3}
-                :cols [:baz]}]}
-
-    ;; TRASH CASES
-
-    ;; Overlap between match-by and ponders
-    {:match-by [:foo :bar]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}
-     {:foo 1 :bar 4}]
-    [{:foo 1 :bar 2}]
-    :=> {:ins [{:foo 1 :bar 1}
-               {:foo 1 :bar 4}]
-         :del [{:foo 1 :bar 2}]}
-
-    ;; Match-by cols missing in rows
-    {:match-by [:baz]
-     :ponders  {:bar 1}}
-    [{:foo 1 :bar 1}
-     {:foo 1 :bar 4}]
-    [{:foo 1 :bar 2}]
-    :=> {:ins [{:foo 1 :bar 4}]
-         :upd [{:src  {:foo 1 :bar 1}
-                :tgt  {:foo 1 :bar 2}
-                :cols [:bar]}]}
-
-    ;; Ponder cols missing in all rows
-    {:match-by [:foo]
-     :ponders  {:baz 1}}
-    [{:foo 1 :bar 1}
-     {:foo 1 :bar 4}]
-    [{:foo 1 :bar 2}]
-    :=> {:ins [{:foo 1 :bar 4}]}
-
-    ;; Ponder cols missing in some rows
-    {:match-by [:foo]
-     :ponders  {:baz 1}}
-    [{:foo 1 :bar 1 :baz 4}
-     {:foo 1 :bar 4}]
-    [{:foo 1 :bar 2}
-     {:foo 1 :bar 7}]
-    :=> {:upd [{:src  {:foo 1 :bar 1 :baz 4}
-                :tgt  {:foo 1 :bar 7}
-                :cols [:baz]}]})
-
-  ;; Match-by is REQUIRED
-  (is (thrown? ArityException (diff nil)))
-  (is (thrown? ArityException (diff {})))
-  (is (thrown? ArityException (diff {:match-by []})))
-
-  ;; TRANSDUCER
-
-  (are [xf src-tgt _ ret]
-      (= (sort-by hash ret)
-         (sort-by hash (into [] xf src-tgt)))
-
-    (diff {:match-by [:foo]
-           :ponders  {:bar 1 :baz 2}})
-    [[[{:id  1 :foo 1 :bar 1 :baz 1}]
-      [{:id 11 :foo 1 :bar 1 :baz 1}]]]
-    :=> []
-
-    (diff {:match-by [:foo]
-           :ponders  {:bar 1 :baz 2}})
-    [[[{:id  1 :foo 1 :bar 1 :baz 1}
+      ;; Some eq rows
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
        {:id  2 :foo 2 :bar 2 :baz 2}]
       [{:id 11 :foo 1 :bar 1 :baz 3}
-       {:id 22 :foo 2 :bar 2 :baz 2}]]]
-    :=> [{:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                 :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
-                 :cols [:baz]}]}]
+       {:id 22 :foo 2 :bar 2 :baz 2}]
+      :=> {:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
+                  :cols [:baz]}]}
 
-    (diff {:match-by [:foo :bar]
-           :ponders  {:baz 2}})
-    [[[{:id  1 :foo 1 :bar 1 :baz 1}
+      ;; No eq rows
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
+       {:id  2 :foo 2 :bar 2 :baz 2}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 2 :bar 4 :baz 2}]
+      :=> {:upd [{:src  {:id  2 :foo 2 :bar 2 :baz 2}
+                  :tgt  {:id 22 :foo 2 :bar 4 :baz 2}
+                  :cols [:bar]}
+                 {:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
+                  :cols [:bar :baz]}]}
+
+      ;; Different count
+      {:match-by [:foo]
+       :ponders  {:bar 1 :baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
+       {:id  2 :foo 2 :bar 2 :baz 2}
+       {:id  3 :foo 2 :bar 4 :baz 2}]
+      [{:id 11 :foo 1 :bar 3 :baz 3}
+       {:id 22 :foo 2 :bar 4 :baz 2}]
+      :=> {:ins [{:id 2 :foo 2 :bar 2 :baz 2}]
+           :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 11 :foo 1 :bar 3 :baz 3}
+                  :cols [:bar :baz]}]}
+
+      ;; Multiple match-by cols
+      {:match-by [:foo :bar]
+       :ponders  {:baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
        {:id  2 :foo 2 :bar 1 :baz 2}
        {:id  3 :foo 2 :bar 4 :baz 2}]
       [{:id 11 :foo 1 :bar 1 :baz 3}
-       {:id 22 :foo 2 :bar 4 :baz 2}]]]
-    :=> [{:ins [{:id 2 :foo 2 :bar 1 :baz 2}]}
-         {:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                 :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
-                 :cols [:baz]}]}]
+       {:id 22 :foo 2 :bar 4 :baz 2}]
+      :=> {:ins [{:id 2 :foo 2 :bar 1 :baz 2}]
+           :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
+                  :cols [:baz]}]}
 
-    (diff {:match-by [:foo :bar]
-           :ponders  {:baz 2}})
-    [[[{:id  1 :foo 1 :bar 1 :baz 1}
-       {:id  2 :foo 1 :bar 1 :baz 2}
-       {:id  3 :foo 1 :bar 1 :baz 2}]
+      ;; Duplicate rows
+      {:match-by [:foo :bar]
+       :ponders  {:baz 2}}
+      [{:id  1 :foo 1 :bar 1 :baz 1}
+       {:id  2 :foo 2 :bar 1 :baz 2}
+       {:id  2 :foo 2 :bar 1 :baz 2}
+       {:id  2 :foo 2 :bar 1 :baz 2}]
       [{:id 11 :foo 1 :bar 1 :baz 3}
        {:id 22 :foo 2 :bar 4 :baz 2}
-       {:id 33 :foo 2 :bar 4 :baz 2}
-       {:id 44 :foo 3 :bar 4 :baz 2}]]]
-    :=> [{:ins [{:id 2 :foo 1 :bar 1 :baz 2}
-                {:id 3 :foo 1 :bar 1 :baz 2}]
-          :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
-                 :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
-                 :cols [:baz]}]}
-         {:del [{:id 22 :foo 2 :bar 4 :baz 2}
-                {:id 33 :foo 2 :bar 4 :baz 2}]}
-         {:del [{:id 44 :foo 3 :bar 4 :baz 2}]}]))
+       {:id 33 :foo 2 :bar 1 :baz 2}]
+      :=> {:ins [{:id  2 :foo 2 :bar 1 :baz 2}
+                 {:id  2 :foo 2 :bar 1 :baz 2}]
+           :del [{:id 22 :foo 2 :bar 4 :baz 2}]
+           :upd [{:src {:id  1 :foo 1 :bar 1 :baz 1}
+                  :tgt {:id 11 :foo 1 :bar 1 :baz 3}
+                  :cols [:baz]}]}))
+
+  (testing "Trash cases"
+    (are [config src tgt _ ret]
+        (st/with-instrument-disabled
+          (test-diff config src tgt ret))
+
+      ;; NONE TO NONE
+
+      {:match-by [:foo]}                   nil nil :=> {}
+      {:match-by [:foo]}                   []  nil :=> {}
+      {:match-by [:foo] :ponders {:bar 1}} nil nil :=> {}
+      {:match-by [:foo] :ponders {:bar 1}} []  nil :=> {}
+
+      ;; SEQ TO NONE
+
+      {:match-by [:foo]}
+      [{:foo 1 :bar 1}]
+      nil
+      :=> {:ins [{:foo 1 :bar 1}]}
+
+      {:match-by [:foo]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}]
+      nil
+      :=> {:ins [{:foo 1 :bar 1}]}
+
+      {:match-by [:bar]}
+      [{:foo 1 :bar 1}
+       {:foo 2 :bar 2}]
+      nil
+      :=> {:ins [{:foo 1 :bar 1}
+                 {:foo 2 :bar 2}]}
+
+      {:match-by [:bar]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}
+       {:foo 2 :bar 2}]
+      nil
+      :=> {:ins [{:foo 1 :bar 1}
+                 {:foo 2 :bar 2}]}
+
+      ;; MANY TO SEQ
+
+      ;; Overlap between match-by and ponders
+      {:match-by [:foo :bar]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}
+       {:foo 1 :bar 4}]
+      [{:foo 1 :bar 2}]
+      :=> {:ins [{:foo 1 :bar 1}
+                 {:foo 1 :bar 4}]
+           :del [{:foo 1 :bar 2}]}
+
+      ;; Match-by cols missing in rows
+      {:match-by [:baz]
+       :ponders  {:bar 1}}
+      [{:foo 1 :bar 1}
+       {:foo 1 :bar 4}]
+      [{:foo 1 :bar 2}]
+      :=> {:ins [{:foo 1 :bar 4}]
+           :upd [{:src  {:foo 1 :bar 1}
+                  :tgt  {:foo 1 :bar 2}
+                  :cols [:bar]}]}
+
+      ;; Ponder cols missing in all rows
+      {:match-by [:foo]
+       :ponders  {:baz 1}}
+      [{:foo 1 :bar 1}
+       {:foo 1 :bar 4}]
+      [{:foo 1 :bar 2}]
+      :=> {:ins [{:foo 1 :bar 4}]}
+
+      ;; Ponder cols missing in some rows
+      {:match-by [:foo]
+       :ponders  {:baz 1}}
+      [{:foo 1 :bar 1 :baz 4}
+       {:foo 1 :bar 4}]
+      [{:foo 1 :bar 2}
+       {:foo 1 :bar 7}]
+      :=> {:upd [{:src  {:foo 1 :bar 1 :baz 4}
+                  :tgt  {:foo 1 :bar 7}
+                  :cols [:baz]}]})
+
+    ;; Match-by is REQUIRED
+
+    (st/with-instrument-disabled
+      (is (thrown? ArityException (diff nil)))
+      (is (thrown? ArityException (diff {})))
+      (is (thrown? ArityException (diff {:match-by []})))))
+
+  (testing "Transducer"
+    (are [xf src-tgt _ ret]
+        (= (sort-by hash ret)
+           (sort-by hash (into [] xf src-tgt)))
+
+      (diff {:match-by [:foo]
+             :ponders  {:bar 1 :baz 2}})
+      [[[{:id  1 :foo 1 :bar 1 :baz 1}]
+        [{:id 11 :foo 1 :bar 1 :baz 1}]]]
+      :=> []
+
+      (diff {:match-by [:foo]
+             :ponders  {:bar 1 :baz 2}})
+      [[[{:id  1 :foo 1 :bar 1 :baz 1}
+         {:id  2 :foo 2 :bar 2 :baz 2}]
+        [{:id 11 :foo 1 :bar 1 :baz 3}
+         {:id 22 :foo 2 :bar 2 :baz 2}]]]
+      :=> [{:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                   :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
+                   :cols [:baz]}]}]
+
+      (diff {:match-by [:foo :bar]
+             :ponders  {:baz 2}})
+      [[[{:id  1 :foo 1 :bar 1 :baz 1}
+         {:id  2 :foo 2 :bar 1 :baz 2}
+         {:id  3 :foo 2 :bar 4 :baz 2}]
+        [{:id 11 :foo 1 :bar 1 :baz 3}
+         {:id 22 :foo 2 :bar 4 :baz 2}]]]
+      :=> [{:ins [{:id 2 :foo 2 :bar 1 :baz 2}]}
+           {:upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                   :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
+                   :cols [:baz]}]}]
+
+      (diff {:match-by [:foo :bar]
+             :ponders  {:baz 2}})
+      [[[{:id  1 :foo 1 :bar 1 :baz 1}
+         {:id  2 :foo 1 :bar 1 :baz 2}
+         {:id  3 :foo 1 :bar 1 :baz 2}]
+        [{:id 11 :foo 1 :bar 1 :baz 3}
+         {:id 22 :foo 2 :bar 4 :baz 2}
+         {:id 33 :foo 2 :bar 4 :baz 2}
+         {:id 44 :foo 3 :bar 4 :baz 2}]]]
+      :=> [{:ins [{:id 2 :foo 1 :bar 1 :baz 2}
+                  {:id 3 :foo 1 :bar 1 :baz 2}]
+            :upd [{:src  {:id  1 :foo 1 :bar 1 :baz 1}
+                   :tgt  {:id 11 :foo 1 :bar 1 :baz 3}
+                   :cols [:baz]}]}
+           {:del [{:id 22 :foo 2 :bar 4 :baz 2}
+                  {:id 33 :foo 2 :bar 4 :baz 2}]}
+           {:del [{:id 44 :foo 3 :bar 4 :baz 2}]}])))
