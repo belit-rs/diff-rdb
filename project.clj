db04bd46118c13f7de317dfbaa5734883dedb134
nikolavojicic <nikolavojicic@outlook.com>
2021-11-18 13:22:09 +0100

    Update deps; add criterium

--- project.clj
+++ project.clj
@@ -6,11 +6,12 @@
   :url "https://github.com/belit-rs/diff-rdb"
   :license {:url "https://github.com/belit-rs/diff-rdb/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.3.622"]
+                 [org.clojure/core.async "1.4.627"]
                  [seancorfield/next.jdbc "1.2.659"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "1.1.0"]
                              [expound "0.8.10"]
                              [orchestra "2021.01.01-1"]
-                             [org.postgresql/postgresql "42.2.23"]]
+                             [org.postgresql/postgresql "42.3.1"]
+                             [criterium "0.4.6"]]
               :jvm-opts ["-Dclojure.core.async.go-checking=true"]}})
