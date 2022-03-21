50537c39a96eb6a3616f88cf4928aa002a7e732e
nikolavojicic <nikolavojicic@outlook.com>
2022-03-21 16:51:48 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :url "https://github.com/belit-rs/diff-rdb"
   :license {:url "https://github.com/belit-rs/diff-rdb/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.4.627"]
+                 [org.clojure/core.async "1.5.648"]
                  [seancorfield/next.jdbc "1.2.659"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/test.check "1.1.0"]
-                             [expound "0.8.10"]
+             {:dependencies [[org.clojure/test.check "1.1.1"]
+                             [expound "0.9.0"]
                              [orchestra "2021.01.01-1"]
-                             [org.postgresql/postgresql "42.3.1"]
+                             [org.postgresql/postgresql "42.3.3"]
                              [criterium "0.4.6"]]
               :jvm-opts ["-Dclojure.core.async.go-checking=true"]}})
