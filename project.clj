c1834dcf33351b5af645699aa032ef57bae56ace
nikolavojicic <nikolavojicic@outlook.com>
2025-04-26 01:36:19 +0200

    Update versions

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
   :description "Tools and APIs for finding, inspecting and fixing differences between data sets in relational databases."
   :url "https://github.com/belit-rs/diff-rdb"
   :license {:url "https://github.com/belit-rs/diff-rdb/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.5.648"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/core.async "1.8.741"]
                  [seancorfield/next.jdbc "1.2.659"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "1.1.1"]
                              [expound "0.9.0"]
                              [orchestra "2021.01.01-1"]
-                             [com.h2database/h2 "2.1.210"]
+                             [com.h2database/h2 "2.3.232"]
                              [criterium "0.4.6"]]
               :jvm-opts ["-Dclojure.core.async.go-checking=true"]}})
